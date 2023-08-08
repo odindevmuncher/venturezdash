7c5966744ddd0f0d08c760720720629fc3a4527a
Odin Lund <dev.38@Odins-MacBook-Pro.local>
2023-08-08 15:35:32 +0200

    first-dash

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'SpaceDrive',
+        productNumber: 'Software',
+        paymentStatus: 'London, UK',
+        status: 'Seed | 1M'
+    },
+    {
+        productName: 'Threads',
+        productNumber: 'Social App',
+        paymentStatus: 'Paris, FR',
+        status: 'Series A | 6M'
+    },
+    {
+        productName: 'Beem',
+        productNumber: 'Metaverse',
+        paymentStatus: 'London, UK',
+        status: 'Seed | 2M'
+    },
+]
